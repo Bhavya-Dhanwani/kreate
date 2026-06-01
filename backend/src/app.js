f92e9831def165439ae63a53a73d23251c24e79b
Bhavya-is-great <bdisop1234@gmail.com>
2026-06-01 10:36:40 +0530

    Made the main router

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import connectDB from "./config/db.config.js";
 import cookieParser from "cookie-parser";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import indexRouter from "./routes/index.route.js";
 
 // Initializing app
 const app = express();
@@ -13,6 +14,9 @@ connectDB();
 // Adding the middleware
 app.use(express.json());
 app.use(cookieParser());
+
+// Adding the index router
+app.use("/api", indexRouter);
 
 // Adding the error middlewares
 app.use(errorMiddleware);
---
+++ backend/src/routes/index.route.js
@@ -0,0 +1,7 @@
+// Importing moduels
+import express from "express";
+
+// Configuring the index Router
+const indexRouter = express.Router();
+
+export default indexRouter;
