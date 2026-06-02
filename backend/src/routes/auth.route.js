e84e23a6f765c7d4b5279484344564cd681eb220
Bhavya-is-great <bdisop1234@gmail.com>
2026-06-02 10:40:51 +0530

    Added the auth route

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,7 @@
+// Importing the modules
+import express from "express";
+
+// Making the router
+const authRouter = express.Router();
+
+export default authRouter;
--- backend/src/routes/index.route.js
+++ backend/src/routes/index.route.js
@@ -1,7 +1,11 @@
 // Importing moduels
 import express from "express";
+import authRouter from "./auth.route.js";
 
 // Configuring the index Router
 const indexRouter = express.Router();
+
+// Adding the branch routes
+indexRouter.use("/auth", authRouter);
 
 export default indexRouter;
